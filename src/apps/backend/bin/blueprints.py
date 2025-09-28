5618ca67a210e627b3e4b8e13c11d41fb740c4c7
Priyanshu Singh <priyanshu.singh1465@.com>
2025-09-28 23:18:19 +0530

    Keep only frontend for Task 2

--- src/apps/backend/bin/blueprints.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-from bin.react_blueprint import react_blueprint, img_assets_blueprint  # import from separate file
-
-# API blueprint
-api_blueprint = Blueprint("api", __name__, url_prefix="/api")
